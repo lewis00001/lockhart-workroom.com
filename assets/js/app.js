97f7d21cf1728dfb406c844fe07227a6d84daf9a
Lewis Lockhart <lewis00001@gmail.com>
2019-12-13 21:46:53 -0700

    added script/css to generate recent work cards

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+$(document).ready( function () {
+
+    // hid all content to start
+    $(".work, .bio, .contact, .tools").hide();
+
+    //
+    $("#work").on("click", function () {
+        $(".work").html("");
+        $(".bio, .contact, .tools").hide();
+        $(".work").show();
+        for (let i = 0; i < myWork.w.length; i++) {
+            $("<div/>", {
+                class: "work-img bloom",
+                html: "<div class='w-title'>" + myWork.w[i].name + "</div>",
+            }).css("background-image", "url('" + myWork.w[i].image + "')").appendTo(".work");
+        }
+        $(".work").prepend("<div class='section-title bloom'>recent work</div>");
+    });
+
+    //
+    $("#bio").on("click", function () {
+        $(".bio").html("");
+        $(".work, .contact, .tools").hide();
+        $(".bio").show();
+        
+        $(".bio").prepend("<div class='section-title bloom'>biography</div>");
+    });
+
+    //
+    $("#contact").on("click", function () {
+        $(".contact").html("");
+        $(".work, .bio, .tools").hide();
+        $(".contact").show();
+        
+        $(".contact").prepend("<div class='section-title bloom'>contact me</div>");
+    });
+
+    //
+    $("#tools").on("click", function () {
+        $(".tools").html("");
+        $(".work, .bio, .contact").hide();
+        $(".tools").show();
+        
+        $(".tools").prepend("<div class='section-title bloom'>helpful tools</div>");
+    });
+
+
+});
---
+++ assets/js/support.js
@@ -0,0 +1,18 @@
+let myWork = {
+    w: [{
+            image: "assets/images/battlePuffs.PNG",
+            name: "Battle Puffs",
+            url: ""
+        },
+        {
+            image: "assets/images/glyphTrivia.PNG",
+            name: "Maya-glyph-trivia",
+            url: ""
+        },
+        {
+            image: "assets/images/RPS.PNG",
+            name: "Rock, Paper, Scissors",
+            url: ""
+        }
+    ]
+};
