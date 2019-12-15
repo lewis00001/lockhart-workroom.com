bc2238028be335f0f522fbe26d282cbb7f281aed
Lewis Lockhart <lewis00001@gmail.com>
2019-12-14 23:24:13 -0700

    added actual links and project scrolling from within the modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ $(document).ready( function () {
     $(".work, .bio, .contact, .tools").hide();
     $(".modal").hide();
 
-    //
+    // creates all the project cards on the recent work page
     $("#work").on("click", function () {
         $(".work").html("");
         $(".bio, .contact, .tools").hide();
@@ -52,16 +52,59 @@ $(document).ready( function () {
         workModal(chosenName);
     });
 
+    // modal index used for changing between projects from the modal
+    let m_index;
+
+    // listens for left click within the modal nav
+    $(document).on("click", ".fa-chevron-circle-left", function () {
+        if (m_index === 0) {
+            m_index = myWork.w.length - 1;
+            let mLeft = myWork.w[m_index].name;
+            workModal(mLeft);
+        } else {
+            m_index--;
+            let mLeft = myWork.w[m_index].name;
+            workModal(mLeft);
+        }
+    });
+
+    // listens for right click within the modal nav
+    $(document).on("click", ".fa-chevron-circle-right", function () {
+        if (m_index === (myWork.w.length - 1)) {
+            m_index = 0;
+            let mRight = myWork.w[m_index].name;
+            workModal(mRight);
+        } else {
+            m_index++;
+            let mRight = myWork.w[m_index].name;
+            workModal(mRight);
+        }
+    });
+
     // work info modal
     function workModal(name) {
-        $(".modal").show();
+        $(".modal").show();                
+        $(".modal-links").html("");
         let modalInfo;
         for (let i = 0; i < myWork.w.length; i++) {
             if (myWork.w[i].name === name) {
+                m_index = i;
                 modalInfo = myWork.w[i];
-                $(".modal-content").html("<img class='modal-img' src='" + modalInfo.image + "'>");
+                $(".modal-title").text(modalInfo.name);
+                $(".modal-image").html("<img class='modal-img' src='" + modalInfo.image + "'>");
+                $(".description-text").text(modalInfo.description);
+                $(".modal-links").append("<div class='modal-link-title m-head'>- Site Links -</div>");
+                $(".modal-links").append("<a href='" + modalInfo.proj_url + "' target='_blank'" +
+                    "<div class='proj-url m-link'>GitHub Repo</div></a>");
+                $(".modal-links").append("<a href='" + modalInfo.hosted_url + "' target='_blank'" +
+                    "<div class='hosted-url m-link'>GitHub Repo</div></a>");
             }
         }
     }
+
+    // closes the modal (X)
+    $(document).on("click", ".close-modal", function () {
+        $(".modal").hide();
+    });
 
 });
--- assets/js/support.js
+++ assets/js/support.js
@@ -2,20 +2,29 @@ let myWork = {
     w: [{
             image: "assets/images/battlePuffs.PNG",
             name: "Battle Puffs",
-            proj_url: "",
-            hosted_url: ""
+            proj_url: "https://github.com/lewis00001/BattlePuffs",
+            hosted_url: "https://lewis00001.github.io/BattlePuffs/",
+            description: "awesome puff gamegsdf gsdfg sdfgdfgs dfgsdfgsdfgsd hdhgesgrsghiser hshbvudryb v sdhb" +
+            "sdfiguh sdghsldkfjghsdlkjfgh lskdjfhgs idufgh islu gskjd fgnsiuth blgskjdfghsidugb skdg" + 
+            "ksjdfhu asdfhsibkjb iy bkjv vbsldhfb sdfiyv bsljfdvsoiuyvslbkdjrvn  lisdrvlsbkdjblskdjvv sfv kfvnlkf" +
+            "sdfiguh sdghsldkfjghsdlkjfgh lskdjfhgs idufgh islu gskjd fgnsiuth blgskjdfghsidugb skdg" + 
+            "sdfiguh sdghsldkfjghsdlkjfgh lskdjfhgs idufgh islu gskjd fgnsiuth blgskjdfghsidugb skdg" + 
+            "sdfiguh sdghsldkfjghsdlkjfgh lskdjfhgs idufgh islu gskjd fgnsiuth blgskjdfghsidugb skdg" + 
+            "sdfiguh sdghsldkfjghsdlkjfgh lskdjfhgs idufgh islu gskjd fgnsiuth blgskjdfghsidugb skdg"
         },
         {
             image: "assets/images/glyphTrivia.PNG",
             name: "Maya-glyph-trivia",
-            proj_url: "",
-            hosted_url: ""
+            proj_url: "https://github.com/lewis00001/TriviaGame",
+            hosted_url: "https://lewis00001.github.io/TriviaGame/",
+            description: "cool glyph game"
         },
         {
             image: "assets/images/RPS.PNG",
             name: "Rock, Paper, Scissors",
-            proj_url: "",
-            hosted_url: ""
+            proj_url: "https://github.com/lewis00001/P.R.S",
+            hosted_url: "https://lewis00001.github.io/P.R.S/",
+            description: "a great RPS game"
         }
     ]
 };
