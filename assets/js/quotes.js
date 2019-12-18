75f35a9b77fbacc2520cd67348f6b4467aaa81ec
Lewis Lockhart <lewis00001@gmail.com>
2019-12-17 18:04:56 -0700

    site was removed - readding all content

---
+++ assets/js/quotes.js
@@ -0,0 +1,48 @@
+let quotes = {
+    q: [
+        {
+            quote: "Lewis is very business minded, and excited to try new ideas " + 
+            "and ventures. He is reliable and self motivated.",
+            source: "Bryce | TravelPass Group"
+        },
+        {
+            quote: "Lewis is \"so nice, so helpful, so informative and funny.\"",
+            source: "Sylvia | Norwak Law Group"
+        },
+        {
+            quote: "Lewis went way above and beyond and I just cannot say enough great things about him.",
+            source: "Amy | Duke Energy"
+        },
+        {
+            quote: "Lewis is a highly focused and motivated individual. I know him to be very creative " +
+            "in his thinking and in his problem solving skills. He would be an asset on any team. I believe " + 
+            "he is a man with a high standard of ethics and is honest in every thing he does.",
+            source: "Robert | Attostat, Inc."
+        },
+        {
+            quote: "Lewis is a honest, friendly, outgoing person. He values customers and goes the extra mile " +
+            "to help them and to make sure they are satisfied. He uses data to help make decisions and is an " +
+            "excellent problem solver.",
+            source: "Tyler | SimpleNexus"
+        },
+        {
+            quote: "Lewis is a great employee. He is detail oriented and a natural leader to those around him. " +
+            "He brings a focused energy and excitement to the team that is really hard to find.",
+            source: "Nathan | States Title"
+        }
+    ],
+    bio: [
+        "I am looking for an entry-level position as a full-stack web developer or a QAA engineer. I've spent " + 
+        "the last six years working at a SaaS company called Simplifile where we provide e-recording services " + 
+        "for documents such as deeds and mortgages.",
+
+        "I began at Simplifile as part of the customer support team and after a year was promoted to the role of " +
+        "team lead. From there I joined the quality assurance team where I've served as a test engineer for the " +
+        "past two years. I work closely with our developers and product managers in a Scrum environment, deploying " +
+        "frequent enhancements and bug fixes.",
+
+        "I have a background in art and graphic design and seamlessly incorporate this with my love of logic and " + 
+        "systems. In my off time I enjoy creating board games, video games, coded alphabets, and tinkering with my " + 
+        "laser cutter and cnc machines."
+    ]
+}
