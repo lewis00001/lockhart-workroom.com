1849d7b3eb599e4090eff617f8815bf022891796
Lewis Lockhart <lewis00001@gmail.com>
2019-12-19 22:13:50 -0700

    I might have added php style for real this time

---
+++ style.php
@@ -0,0 +1,26 @@
+<?php
+
+header("Content-type: text/css");
+
+.e-conf {
+    margin: auto;
+    margin: 40px;
+    padding: 10px;
+    width: 250px;
+    background-color: rgb(43, 80, 110);
+}
+
+.e-text {
+    color: white;
+    font-family: 'Raleway', sans-serif;
+    font-size: 18px;
+}
+
+.e-button {
+    font-family: 'Raleway', sans-serif;
+    border: 1px solid black;
+    font-weight: 800;
+    color: black;
+    font-size: 18px;
+    background-color: gray;
+}
