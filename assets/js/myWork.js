0fa4b0389fc2a5219e8c55de8936e2b9cc1e705e
Lewis Lockhart <lewis00001@gmail.com>
2020-05-29 17:35:04 -0600

    added dev languages to game descriptions

--- assets/js/myWork.js
+++ assets/js/myWork.js
@@ -4,7 +4,8 @@ let myWork = {
             name: "Battle Puffs",
             proj_url: "https://github.com/lewis00001/BattlePuffs",
             hosted_url: "https://lewis00001.github.io/BattlePuffs/",
-            description: "Battle Puffs features four cute puffs, from which " + 
+            description: "Dev: jQuery, Javascript, CSS, HTML :: " + 
+                "Battle Puffs features four cute puffs, from which " + 
                 "the player selects one to battle with. The selected puff will need to " + 
                 "battle the remaining three to win the game. Each stat and name is generated " + 
                 "randomly to add diversity to the game play. The selected puff's attack grows " + 
@@ -16,7 +17,8 @@ let myWork = {
             name: "Maya-glyph-trivia",
             proj_url: "https://github.com/lewis00001/TriviaGame",
             hosted_url: "https://lewis00001.github.io/TriviaGame/",
-            description: "This glyph trivia game contains 24 Maya glyphs, Eight of which are " + 
+            description: "Dev: jQuery, Javascript, CSS, HTML :: " +
+                "This glyph trivia game contains 24 Maya glyphs, Eight of which are " + 
                 "selected at random for the player. The player must choose the correct answer out " +
                 "of the four possible options. Each question is timed. At the completion of the game " +
                 "the player is presented with the number of questions they got correct, incorrect, and " +
@@ -27,7 +29,8 @@ let myWork = {
             name: "Rock, Paper, Scissors",
             proj_url: "https://github.com/lewis00001/P.R.S",
             hosted_url: "https://lewis00001.github.io/P.R.S/",
-            description: "This rock, paper, scissors game allows two players to lock in a position while " + 
+            description: "Dev: Firebase, jQuery, Javascript, CSS, HTML :: " +
+                "This rock, paper, scissors game allows two players to lock in a position while " + 
                 "others can watch the game and chat in comments. The game tracks each players status, selections, " + 
                 "and wins using Firebase. The players and the visitors can all communicate through the provided chat " + 
                 "feature. The chat allows users to select unique colors for their username and chat text."
@@ -37,19 +40,21 @@ let myWork = {
             name: "JiffyGif",
             proj_url: "https://github.com/lewis00001/Giphy_API",
             hosted_url: "https://lewis00001.github.io/Giphy_API/",
-            description: "A basic gif search API page. Allows users to select number of results, and ratings. Searches " + 
-            "are automatically added to the searches section. Searches can be added and removed from the favorites section. " + 
-            "Results are returned with the gif name and rating information."
+            description: "Dev: Ajax, API, jQuery, Javascript, CSS, HTML :: " + 
+                "A basic gif search API page. Allows users to select number of results, and ratings. Searches " + 
+                "are automatically added to the searches section. Searches can be added and removed from the favorites section. " + 
+                "Results are returned with the gif name and rating information."
         },
         {
             image: "assets/images/tempic.PNG",
             name: "Tempic",
             proj_url: "https://github.com/lewis00001/tempic",
             hosted_url: "https://lewis00001.github.io/tempic/",
-            description: "Five images will be presented to the user randomly from a list of 20 possible cities. " + 
-            "The user will need to order them by coldest to warmest. They will have a limited number of tries to " +
-            "get it correct. Current temperatures will be obtained via a weather API. General information about each " + 
-            "city will be provided by a wiki API and available to the user via an additional information button."
+            description: "Dev: Ajax, API, jQuery, Javascript, CSS, HTML :: " +
+                "Five images will be presented to the user randomly from a list of 20 possible cities. " + 
+                "The user will need to order them by coldest to warmest. They will have a limited number of tries to " +
+                "get it correct. Current temperatures will be obtained via a weather API. General information about each " + 
+                "city will be provided by a wiki API and available to the user via an additional information button."
         }
     ]
 };
